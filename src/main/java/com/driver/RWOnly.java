bfc2bea5d1cf87b51e63109c2e7421ebec1b6d68
Khushal Nagmote <125303323+Codechefkhush@users.noreply.github.com>
2023-06-18 11:27:05 +0530

    Added solution

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+       public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        //obj.name = "Kunal";
+        obj.setName("Kunal");
+        obj.getName();
+    }
 }
